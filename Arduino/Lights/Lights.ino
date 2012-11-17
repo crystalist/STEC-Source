9aee83152b684a5ab2c5bc4cde72ac2d7209e2ce
Tacksoo Im <tacksoo.im@gmail.com>
2012-11-17 15:15:26 -0500

    simple arduino example

---
+++ Arduino/Lights/Lights.ino
@@ -0,0 +1,26 @@
+int led = 9;
+
+void setup() {
+  pinMode(led, OUTPUT);
+  Serial.begin(9600); 
+}
+
+void loop() {
+  
+  while(Serial.available() == 0);
+  
+  int val = Serial.read();
+  Serial.println(val);
+  
+  if( val == 1)
+  {
+     Serial.println("Led ON");
+     digitalWrite(led,HIGH); 
+  }
+  else if( val == 0)
+  {
+     Serial.println("Led OFF");
+     digitalWrite(led,LOW); 
+  }
+  
+}
---
+++ ArduinoJava/SendCommandsThread.java
@@ -0,0 +1,36 @@
+package ArduinoJava;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Scanner;
+
+/**
+ * Simple thread to turn on/off an led
+ * 
+ * @author dr.im
+ *
+ */
+public class SendCommandsThread extends Thread {
+
+	private OutputStream output;
+
+	public SendCommandsThread(OutputStream ostream) {
+		output = ostream;
+	}
+
+	@Override
+	public void run() {
+		Scanner keyboard = new Scanner(System.in);
+		
+		while(true)
+		{
+			System.out.println("Please type 1 on turn-on the lights 0 to turn-off");
+			int command = keyboard.nextInt();
+			try {
+				output.write(command);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
